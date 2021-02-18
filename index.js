d82a6966974710c84944d5640e15ff24b5e9d821
hermtechs <hermtechs@gmail.com>
2021-02-18 22:48:38 +0300

    responsive nav menu without js

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-const video = document.querySelectorAll('#video-element');
-const playPauseBtn = document.querySelectorAll('.play-btn');
-playPauseBtn[0].addEventListener('click', ()=>{
-    if(video[0].paused){
-        playPauseBtn[0].classList.add('pause-btn');
-        video[0].play();
-    }
-    else{
-        video[0].pause();
-        playPauseBtn[0].classList.remove('pause-btn');
-    }
-   
-})
+const playPauseBtn = document.querySelectorAll(".play-btn");
+
+//ENABLING THE VIDEOS TO BE PLAYED AND PAUSED
+//getting play-pause btns for each video
+playPauseBtn.forEach((btn) => {
+  btn.addEventListener("click", VideoPlayerController);
+});
+function VideoPlayerController(event) {
+  const ClickedPlayBtn =
+    event.target; /*getting 
+                                specific playBtn*/
+  const VideoToBePlayedContainer = ClickedPlayBtn.parentElement.parentElement;
+
+  //getting specific video to be played
+  const videoToBePlayed = VideoToBePlayedContainer.querySelector(
+    "#video-element"
+  );
+
+  //playing and pausing the videos
+  if (videoToBePlayed.paused) {
+    ClickedPlayBtn.classList.add("pause-btn");
+    videoToBePlayed.play();
+  } else {
+    videoToBePlayed.pause();
+    ClickedPlayBtn.classList.remove("pause-btn");
+  }
+
+  // changing the playBtn from pause back to play after the video has stopped playing
+  videoToBePlayed.onended = () => {
+    ClickedPlayBtn.classList.remove("pause-btn");
+  };
+  //WORKING ON THE VIDEO PROGRESS-BAR
+  videoToBePlayed.addEventListener("timeupdate", changeVideoProgressBar);
+  function changeVideoProgressBar() {
+    const specificVideoProgressBar = VideoToBePlayedContainer.querySelector(
+      ".video-progress-bar"
+    );
+    const videoDuration = videoToBePlayed.duration;
+    const videoProgressValue = videoToBePlayed.currentTime;
+
+    //   setting the video according to currentTime
+    specificVideoProgressBar.style.width =
+      (videoProgressValue / videoDuration) * 100 + "%";
+  }
+}
